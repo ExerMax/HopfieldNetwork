d172efcb778af82d6a93d9790fbd77088104ee05
ExerMax <145705503+ExerMax@users.noreply.github.com>
2024-11-04 20:16:53 +0400

    v1

---
+++ HopfieldNetwork/Hopfield.cs
@@ -0,0 +1,74 @@
+﻿namespace HopfieldNetwork
+{
+    public class Hopfield
+    {
+        private List<int[,]> _matrices = new List<int[,]>();
+        private int _sampleLenght = 0;
+        private int[,] _weightMatrix;
+
+        public Hopfield(int sampleLenght)
+        {
+            _sampleLenght = sampleLenght;
+        }
+
+        public Hopfield AddSample(int[] sample)
+        {
+            if (sample is null) throw new ArgumentNullException();
+            if (sample.Length != _sampleLenght) throw new ArgumentNullException();
+
+            int[,] res = DoMatrix(sample);
+
+            _matrices.Add(res);
+
+            return this;
+        }
+
+        public void DoWeightMatrix()
+        {
+            _weightMatrix = new int[_sampleLenght, _sampleLenght];
+
+            for (int i = 0; i < _sampleLenght; i++)
+            {
+                for (int j = 0; j < _sampleLenght; j++)
+                {
+                    _weightMatrix[i,j] = _matrices.Select(x => x[i,j]).Sum();
+                }
+            }
+        }
+
+        public int[] Compute(int[] sample)
+        {
+            if (sample is null) throw new ArgumentNullException();
+            if (sample.Length != _sampleLenght) throw new ArgumentNullException();
+
+            int[] res = new int[_sampleLenght];
+
+            for (int i = 0; i < _sampleLenght; i++)
+            {
+                for (int j = 0; j < _sampleLenght; j++)
+                {
+                    res[i] += _weightMatrix[i, j] * sample[j];
+                }
+
+                res[i] = res[i] > 0 ? 1 : -1;
+            }
+
+            return res;
+        }
+
+        private int[,] DoMatrix(int[] sample)
+        {
+            int[,] matrix = new int[_sampleLenght, _sampleLenght];
+
+            for (int i = 0; i < _sampleLenght; i++)
+            {
+                for (int j = 0; j < _sampleLenght; j++)
+                {
+                    matrix[i, j] = i == j ? 0 : sample[i] * sample[j];
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
--- HopfieldNetworkConsole/Program.cs
+++ HopfieldNetworkConsole/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HopfieldNetwork;
+
+int[] sample1 = { 1, 1, -1, 1 };
+int[] sample2 = { 1, -1, 1, 1 };
+int[] sample3 = { -1, 1, -1, -1 };
+
+Hopfield hopfield = new Hopfield(4);
+
+hopfield.AddSample(sample1).AddSample(sample2).AddSample(sample3).DoWeightMatrix();
+
+int[] testSample = { 1, -1, 1, -1 };
+
+int[] res = hopfield.Compute(testSample);
+
+foreach (int i in res)
+{
+    Console.Write(i + " ");
+}
